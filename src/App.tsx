fb9779bbfe1248c7da444b1eb4062d2740fa9149
wujiTAO-codes <kathetsumit0@gmail.com>
2025-06-21 14:44:13 +0545

    landing page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-function App() {
-  return (
-    <div className="text-center p-6 text-2xl text-rose-600 font-bold">
-      Hello CHORI 🌸
-    </div>
-  )
-}
-export default App
+
+import { Toaster } from "@/components/toaster";
+import { Toaster as Sonner } from "@/components/sonner";
+import { TooltipProvider } from "@/components/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+// import SignUp from "./pages/SignUp";
+// import Login from "./pages/Login";
+
+// import Dashboard from "./pages/Dashboard";
+
+// import Demo from "./pages/Demo";
+// import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/demo" element={<Demo />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* <Route path="*" element={<NotFound />} />  */}
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
