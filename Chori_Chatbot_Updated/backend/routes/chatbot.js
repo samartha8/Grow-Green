023b55d2c69b5aa9d2cef3d03588190903a46cfd
samartha8 <shakyasamarth1@gmail.com>
2025-06-23 06:32:35 +0545

    chore: remove redundant Chori_Chatbot_Updated directory

--- Chori_Chatbot_Updated/backend/routes/chatbot.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { askChori } = require("../controllers/chatbotController");
-
-router.post("/", askChori);
-
-module.exports = router;
